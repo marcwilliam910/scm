82969d82f4d7dfd351a502962e4661d76fc57a63
marcwilliam910 <marcwilliamvaliente910@gmail.com>
2025-09-19 14:57:49 +0800

    downgrade version

--- src/cloudinary/index.ts
+++ src/cloudinary/index.ts
@@ -1,19 +1,17 @@
-// Try direct require without destructuring
 const cloudinary = require("cloudinary");
 
 const CLOUD_NAME = process.env.CLOUD_NAME!;
 const CLOUD_KEY = process.env.CLOUD_KEY!;
 const CLOUD_SECRET = process.env.CLOUD_SECRET!;
 
-// Access v2 after requiring
-cloudinary.v2.config({
+cloudinary.config({
   cloud_name: CLOUD_NAME,
   api_key: CLOUD_KEY,
   api_secret: CLOUD_SECRET,
   secure: true,
 });
 
-const cloudinaryUploader = cloudinary.v2.uploader;
+const cloudinaryUploader = cloudinary.uploader;
 
-export const cloudinaryApi = cloudinary.v2.api;
+export const cloudinaryApi = cloudinary.api;
 export default cloudinaryUploader;
