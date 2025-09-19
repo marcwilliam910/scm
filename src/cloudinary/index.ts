e3d679e89a53a9cab0fd34df1a903ce18b15e14c
marcwilliam910 <marcwilliamvaliente910@gmail.com>
2025-09-19 14:38:56 +0800

    Fix cloudinary import issue

--- src/cloudinary/index.ts
+++ src/cloudinary/index.ts
@@ -1,17 +1,20 @@
-import {v2 as cloudinary} from "cloudinary";
+import cloudinary from "cloudinary";
+
+// Use v2 directly from the main import
+const cloudinaryV2 = cloudinary.v2;
 
 const CLOUD_NAME = process.env.CLOUD_NAME!;
 const CLOUD_KEY = process.env.CLOUD_KEY!;
 const CLOUD_SECRET = process.env.CLOUD_SECRET!;
 
-cloudinary.config({
+cloudinaryV2.config({
   cloud_name: CLOUD_NAME,
   api_key: CLOUD_KEY,
   api_secret: CLOUD_SECRET,
   secure: true,
 });
 
-const cloudinaryUploader = cloudinary.uploader;
+const cloudinaryUploader = cloudinaryV2.uploader;
 
-export const cloudinaryApi = cloudinary.api;
+export const cloudinaryApi = cloudinaryV2.api;
 export default cloudinaryUploader;
