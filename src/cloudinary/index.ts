f847295eecde147e8dd340e8ae66c2ae0161495b
marcwilliam910 <marcwilliamvaliente910@gmail.com>
2025-09-19 14:42:10 +0800

    Fix cloudinary with CommonJS require

--- src/cloudinary/index.ts
+++ src/cloudinary/index.ts
@@ -1,20 +1,17 @@
-import cloudinary from "cloudinary";
-
-// Use v2 directly from the main import
-const cloudinaryV2 = cloudinary.v2;
+const cloudinary = require("cloudinary").v2;
 
 const CLOUD_NAME = process.env.CLOUD_NAME!;
 const CLOUD_KEY = process.env.CLOUD_KEY!;
 const CLOUD_SECRET = process.env.CLOUD_SECRET!;
 
-cloudinaryV2.config({
+cloudinary.config({
   cloud_name: CLOUD_NAME,
   api_key: CLOUD_KEY,
   api_secret: CLOUD_SECRET,
   secure: true,
 });
 
-const cloudinaryUploader = cloudinaryV2.uploader;
+const cloudinaryUploader = cloudinary.uploader;
 
-export const cloudinaryApi = cloudinaryV2.api;
+export const cloudinaryApi = cloudinary.api;
 export default cloudinaryUploader;
